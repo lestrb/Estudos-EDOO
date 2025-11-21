1403ca7a6908848af2d3678747d68b9345695ea8
Letícia Staudinger <lsr@cin.ufpe.br>
2025-11-21 05:22:34 -0300

    Ajustes

--- List_1/b_fila.cpp
+++ List_1/b_fila.cpp
@@ -11,40 +11,34 @@ class Lista{
         Lista(const Lista&) {}          // Construtor de copia
 
     public:
-        Lista() {} // Construtor virtual padrao
+        Lista() {} // Construtor padrao
         virtual ~Lista() {} // Destrutor virtual padrao
 
         // Metodos virtuais puros
         virtual void limpar() = 0; // Limpa a lista
 
-        //virtual void inserirComeco(const E& item) = 0; // Insere um elemento no comeco da lista
-
         virtual void inserirFim(const E& item) = 0; // Insere um elemento no final da lista
 
         virtual E remover(const E& item) = 0; // Remove um elemento da lista e retorna o elemento removido
 
-        // Navegacao do cursor
-        virtual void proximo() = 0;
+        virtual void proximo() = 0; // Navegacao do cursor para o proximo elemento
 
-        // Informacoes sobre a lista
         virtual int tamanho() const = 0;
-        virtual const E& pegarValor() const = 0;
+
+        virtual const E& pegarValor() const = 0; // Retorna valor atual do cursor
 };
 
-// Implementacao lista ligada, herdando da classe Lista
-template <typename E>
+template <typename E> // Implementacao lista ligada, herdando da classe Lista
 class ListaLigada : public Lista<E> {
     private:
-        // Define o no - struct pq todos os elementos sao publicos para a classe ListaLigada, mas sao encapsulados para o mundo externo
+        // Define o no -> struct pq todos os elementos sao publicos para a classe ListaLigada, mas sao encapsulados para o mundo externo
         struct No{
             E elemento;  // Dado armazenado no no
             No* proximo; // Ponteiro para o proximo no
 
-            // Construtor do no
-            No(const E& elem, No* prox = nullptr) : elemento(elem), proximo(prox) {}
+            No(const E& elem, No* prox = nullptr) : elemento(elem), proximo(prox) {} // Construtor do no
         };
         
-        // Atributos de controle da lista
         No* cabeca; // Ponteiro para o primeiro no da lista
         No* cauda;  // Ponteiro para o ultimo no da lista
         No* atual;  // Ponteiro para o no atual (cursor)
@@ -53,12 +47,11 @@ class ListaLigada : public Lista<E> {
         // Metodo pra inicializar a lista
         void init() {
             cabeca = new No(E(), nullptr); // Cria o no sentinela/dummy
-            cauda = cabeca;  // No inicio, cabeca e cauda sao o mesmo
-            atual = cabeca; // Cursor tambem
+            cauda = cabeca = atual;  // No inicio, cabeca, cauda e cursor sao o mesmo
             tamanhoLista = 0;
         }
 
-        // Metodo auxiliar para deletar tudo (sem deletar a cabeca)
+        // Metodo auxiliar para deletar tudo (menos a cabeca)
         void removerTodos() {
             while(cabeca->proximo != nullptr) {
                 atual = cabeca->proximo;          // Pega o primeiro 
@@ -70,13 +63,11 @@ class ListaLigada : public Lista<E> {
         }
 
         public:
-        // Construtor
-        ListaLigada() {
+        ListaLigada() { // Construtor
             init();
         }
 
-        // Destrutor
-        ~ListaLigada() {
+        ~ListaLigada() { // Destrutor
             limpar();
             delete cabeca;
         }
@@ -85,16 +76,6 @@ class ListaLigada : public Lista<E> {
         void limpar() override {
             removerTodos();
         }
-
-        // void inserirComeco(const E& item) override {
-        //     cabeca->proximo = new No(item, cabeca->proximo); // Aponta pro primeiro no sem ser o dummy
-
-        //     if (cauda == cabeca) {
-        //         cauda = cabeca->proximo;
-        //         atual = cabeca->proximo; // Se for o primeiro elemento, atual aponta pra ele
-        //     }
-        //     tamanhoLista++;
-        // }
 
         void inserirFim(const E& item) override {
             cauda->proximo = new No(item, nullptr); // No apontando para NULL
@@ -134,8 +115,8 @@ class ListaLigada : public Lista<E> {
         }
 
         void proximo() override {
-            if (tamanhoLista == 0) return;   // Se a lista estiver vazia, nao faz nada
-            if (atual->proximo != nullptr) { // Se nao estiver no final, move cursor
+            if (tamanhoLista == 0) return;   // Se a lista tiver vazia, nao faz nada
+            if (atual->proximo != nullptr) { // Se nao tiver no final, move cursor
                 atual = atual->proximo;
             } else { // Se tiver no final, volta pro primiero elemento real
                 atual = cabeca->proximo;
@@ -148,7 +129,7 @@ class ListaLigada : public Lista<E> {
 
         const E& pegarValor() const override {
             if (tamanhoLista == 0) {
-                 return cabeca->elemento; // Retorna valor padrao se a lista estiver vazia
+                 return cabeca->elemento; // Retorna valor padrao se a lista tiver vazia
             }
             if (atual == cabeca) { // Se a lista tem itens mas o cursor esta no dummy, retorna o 1º real
                 return cabeca->proximo->elemento;
@@ -161,7 +142,7 @@ int main(){
     int quantidade_pessoas;
     int tempo_pessoas;
 
-    // Ponteiros do tipo Lista (pai), instanciando a classe ListaLigada (filha)
+    // Ponteiros do tipo Lista (pai), instanciando a classe ListaLigada
     Lista<int>* fila_original = new ListaLigada<int>();
     Lista<int>* fila_ordenada = new ListaLigada<int>();
 
@@ -172,7 +153,7 @@ int main(){
         fila_original->inserirFim(tempo_pessoas);
     };
 
-    // Ordena fila pra ter o minimo de pessoas desapontadas
+    // Fila em ordem crescente (pra ter o minimo de pessoas desapontadas)
     while (fila_original->tamanho() > 0){
         int menor_tempo = fila_original->pegarValor();
         fila_original->proximo();
